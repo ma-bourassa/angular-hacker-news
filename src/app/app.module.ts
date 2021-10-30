8ff51e46ef4baf6e7c3fee92d56d9eb6836bc4cb
Marc-Andre Bourassa <boumar04@gmail.com>
2021-10-29 20:51:11 -0400

    responsive header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HeaderComponent } from 'src/app/layout/header/header.component';
+import { HeaderComponent } from 'src/app/components/layout/header/header.component';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { FooterComponent } from './layout/footer/footer.component';
-import { NewsListComponent } from './news-list/news-list.component';
+import { FooterComponent } from './components/layout/footer/footer.component';
+import { NewsListComponent } from './components/news-list/news-list.component';
 
 @NgModule({
   declarations: [AppComponent, HeaderComponent, FooterComponent, NewsListComponent],
